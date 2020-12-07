d85b03ae86da057ce9747b42ceff8883ae4c00f1
Americo Tomas <americotomaswork@gmail.com>
2020-12-07 23:47:15 +0000

    setup font for the react native app

--- myRNativeCLIapp/.prettierrc.js
+++ myRNativeCLIapp/.prettierrc.js
@@ -1,7 +1,8 @@
 module.exports = {
-  bracketSpacing: false,
-  jsxBracketSameLine: true,
-  singleQuote: true,
-  trailingComma: 'all',
-  tabWidth: 4,
+    bracketSpacing: true,
+    jsxBracketSameLine: true,
+    singleQuote: true,
+    trailingComma: 'all',
+    tabWidth: 4,
+    arrowParens: 'avoid',
 };
