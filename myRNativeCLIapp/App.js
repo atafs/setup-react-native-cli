03894cd63641d61bbd3cf9155fbd0e1da276df60
Americo Tomas <americotomaswork@gmail.com>
2020-12-16 12:44:21 +0000

    run app in android

--- myRNativeCLIapp/App.js
+++ myRNativeCLIapp/App.js
@@ -5,16 +5,13 @@ import fonts from './src/config/fonts';
 const App = () => {
     return (
         <View>
-            <SafeAreaView>
-                <Text style={styles.text}>I am here!!</Text>
-            </SafeAreaView>
+            <Text style={styles.text}>I am here!!</Text>
         </View>
     );
 };
 
 const styles = StyleSheet.create({
     text: {
-        flex: 1,
         fontSize: 28,
         fontFamily: fonts.vincHand,
         padding: 40,
